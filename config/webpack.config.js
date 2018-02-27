33f3475d016d75426febe4b1cde840f6bf39d888
g.lariviere <g.lariviere@qwant.com>
2018-02-27 17:36:47 +0100

    [ADD] release v1.0.1

---
+++ config/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  context: path.resolve(__dirname, '../src'),
+  entry: [path.resolve(__dirname, '../src/index.js')],
+  output: {
+    pathinfo: true, // Add /* filename */ comments to generated require()s in the output.
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, '../node_modules')],
+    extensions: ['.web.js', '.js', '.json', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      components: path.resolve(__dirname, '../src/components'),
+      constants: path.resolve(__dirname, '../src/constants'),
+      assets: path.resolve(__dirname, '../src/assets'),
+    },
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: path.resolve(__dirname, '../src'),
+      exclude: /(node_modules|bower_components|dist)/,
+      use: require.resolve('babel-loader'),
+    }, {
+      test: /\.css$/,
+      include: path.resolve(__dirname, '../src'),
+      exclude: /(node_modules|bower_components|dist)/,
+      use: [{
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 1, // User postcss-loader to handle @import()
+          modules: true, // Enable CSS Modules
+          localIdentName:'[local]_[hash:base64:5]',
+        },
+      }, {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+          sourceMap: true,
+          plugins: () => [
+            require('postcss-flexbugs-fixes'),
+            require('postcss-import')({
+              root: path.resolve(__dirname, '../src/assets'),
+            }),
+            require('postcss-cssnext')(),
+            require('postcss-preset-env')(),
+          ],
+        },
+      }],
+    }],
+  },
+  externals: {
+    'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  }
+}
---
+++ src/components/Button/Button.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './Button.css'
+
+const Button = ({ onClick, label }) =>
+  <button
+    className={styles.Button}
+    onClick={onClick}
+  >
+    <span className={styles.label}>
+      {label}
+    </span>
+  </button>
+
+Button.defaultProps = {
+  onClick: null,
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  label: PropTypes.string.isRequired,
+}
+
+export default Button
